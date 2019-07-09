b41733dc8e82b3616aaba12c0ab51de31315bbfe
Komolafe Olorundara <49000733+Flystunna@users.noreply.github.com>
2019-07-10 00:39:48 +0100

    Add files via upload

---
+++ MyCrud/MyCrud/Controllers/EmployeeController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyCrud.Models;
+using System.Data.Entity.Validation;
+using System.Data.Entity;
+using System.Data;
+using System.Data.EntityClient;
+
+namespace MyCrud.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+
+        public ActionResult GetData() 
+        {
+            using (DBModel db = new DBModel())
+            {
+                List<Employee> empList = db.Employees.ToList<Employee>();
+                return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
+            }
+        
+        }
+        [HttpGet]
+
+        public ActionResult AddorEdit(int id = 0) 
+        {
+            if (id == 0)
+                return View(new Employee());
+            else 
+            {
+                using (DBModel db = new DBModel()) 
+                {
+                    return View(db.Employees.Where(x => x.EmployeeID == id).FirstOrDefault<Employee>());
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddorEdit(Employee emp)
+        {
+            using (DBModel db = new DBModel())
+            {
+                if (emp.EmployeeID == 0)
+                {
+                    db.Employees.Add(emp);
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Saved Successfully" }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    db.Entry(emp).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Updated Successfully" }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            using (DBModel db = new DBModel())
+            {
+                Employee emp = db.Employees.Where(x => x.EmployeeID == id).FirstOrDefault<Employee>();
+                db.Employees.Remove(emp);
+                db.SaveChanges();
+                return Json(new { success = true, message = "Deleted Successfully" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+	}
+}
+
+/*using (DBModel db = new DBModel())
+          {
+              try 
+              {
+
+              db.Employees.Add(emp);
+              db.SaveChanges();
+               
+              }
+
+              catch (DbEntityValidationException e)
+              {
+                    
+                  foreach (var validationErrors in e.EntityValidationErrors)
+                  {
+                      foreach (var validationError in validationErrors.ValidationErrors) 
+                      {
+                          System.Console.WriteLine("Property: {0} Error : {1}", validationError.PropertyName, validationError.ErrorMessage);
+
+                      }
+                  }
+              }
+
+               return Json(new {success = true, message = "Saved Successfully" }, JsonRequestBehavior.AllowGet);
+          }*/
---
+++ MyCrud/MyCrud/Controllers/InventoryController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyCrud.Models;
+using System.Data.Entity.Validation;
+using System.Data.Entity;
+using System.Data;
+using System.Data.EntityClient;
+
+namespace MyCrud.Controllers
+{
+    public class InventoryController : Controller
+    {
+        //
+        // GET: /Inventory/
+        public ActionResult Index()
+        {
+            return View();
+            
+        }
+
+        public ActionResult GetData()
+        {
+            using (DBInventory db = new DBInventory())
+            {
+                List<Inventory> empList = db.Inventories.ToList<Inventory>();
+                return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+
+        [HttpGet]
+
+        public ActionResult Add(int id = 0)
+        {
+            if (id == 0)
+                return View(new Inventory());
+            else
+            {
+                using (DBInventory db = new DBInventory())
+                {
+                    return View(db.Inventories.Where(x => x.InventoryID == id).FirstOrDefault<Inventory>());
+                }
+            }
+        }
+        [HttpPost]
+        public ActionResult Add(Inventory emp)
+        {
+            using (DBInventory db = new DBInventory())
+            {
+                if (emp.InventoryID == 0)
+                {
+                    db.Inventories.Add(emp);
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Saved Successfully" }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    db.Entry(emp).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Updated Successfully" }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            using (DBInventory db = new DBInventory())
+            {
+                Inventory emp = db.Inventories.Where(x => x.InventoryID == id).FirstOrDefault<Inventory>();
+                db.Inventories.Remove(emp);
+                db.SaveChanges();
+                return Json(new { success = true, message = "Deleted Successfully" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+	}
+}
